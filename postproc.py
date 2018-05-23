0138d28b764e741842e34ea3fe5cf1253f522d4c
Shalitha Weerakoon Karunatilleke <sweerakoon@student.unimelb.edu.au>
2018-05-24 06:08:45 +1000

    [Shalitha] WIP

--- postproc.py
+++ postproc.py
@@ -22,6 +22,23 @@ Generates the following graphs:
     Prefix
     _nj: To identify NetLogo doc related attributes
     _j: To identify Java doc related attributes
+
+    Experiments: Punctuated Equilibrium
+    movement off ~ on experiments
+    01 ~ 06
+    02 ~ 07
+    03 ~ 08
+    04 ~ 09
+    05 ~ 10
+
+    Quiescence
+    11
+    12
+    13
+
+    Anarchy
+    14
+    15
 """
 
 import scipy
@@ -32,28 +49,33 @@ import numpy as np
 import seaborn as sns
 
 REBELLION_THRESHOLD = 50  # num active agents to constitue a rebellion
-NETLOGO_DATA_FILE_NAME = "experiments/1_raw_data/Rebellion experiment 06 (NetLogo).csv"
-JAVA_DATA_FILE_NAME = "experiments/1_raw_data/Rebellion experiment 06 (Java).csv"
+NETLOGO_DATA_FILE_NAME = "experiments/1_raw_data/Rebellion experiment 15 (NetLogo).csv"
+JAVA_DATA_FILE_NAME = "experiments/1_raw_data/Rebellion experiment 15 (Java).csv"
 
 # read in the excel file
 print("Reading NetLogo file " + NETLOGO_DATA_FILE_NAME)
-print("Reading Java file " + JAVA_DATA_FILE_NAME)
 df_nl = pd.read_csv(NETLOGO_DATA_FILE_NAME, skiprows=range(0, 21))
 df_nl.drop(df_nl.iloc[:, 0:1], inplace=True, axis=1)
 df_nl.drop(df_nl.iloc[:, 4:8], inplace=True, axis=1)
-print(df_nl)
+print("Reading Java file " + JAVA_DATA_FILE_NAME)
 df_j = pd.read_csv(JAVA_DATA_FILE_NAME, skiprows=range(0, 21))
 df_j.drop(df_j.iloc[:, 0:1], inplace=True, axis=1)
 df_j.drop(df_j.iloc[:, 4:8], inplace=True, axis=1)
-print(df_j)
+
+df_nl_header = pd.read_csv(NETLOGO_DATA_FILE_NAME, skiprows=range(0, 7), nrows=6)
+df_nl_header.drop(df_nl_header.iloc[:, 2:5], inplace=True, axis=1)
+df_j_header = pd.read_csv(JAVA_DATA_FILE_NAME, skiprows=range(0, 7), nrows=6)
+df_j_header.drop(df_j_header.iloc[:, 2:5], inplace=True, axis=1)
 
 print()
 print("======================================")
 print("Summary statistics over all timesteps (NetLogo):")
 print(df_nl.mean(axis=0))
+print(df_nl_header)
 print()
 print("Summary statistics over all timesteps (Java):")
 print(df_j.mean(axis=0))
+print(df_j_header)
 print("======================================")
 print()
 
@@ -83,8 +105,8 @@ def find_rebellion_duration(rebellions):
     for t, actives in rebellions:
         rebellion_duration.append(len(actives))
     print("REBELLION_DURATION ", rebellion_duration)
-    print("Average of REBELLION_DURATION: ", reduce(lambda x, y: x + y, rebellion_duration) / (1.0 *
-                                                                                               len(rebellion_duration)))
+    # print("Average of REBELLION_DURATION: ", reduce(lambda x, y: x + y, rebellion_duration) / (1.0 *
+    #                                                                                            len(rebellion_duration)))
     print()
     return rebellion_duration
 
@@ -94,8 +116,8 @@ def find_rebellion_frequency(rebellions):
     for i in range(1, len(rebellions)):
         rebellion_frequency.append(rebellions[i][0] - rebellions[i - 1][0])
     print("REBELLION_FREQUENCY ", rebellion_frequency)
-    print("Average of REBELLION_FREQUENCY: ",
-          reduce(lambda x, y: x + y, rebellion_frequency) / (1.0 * len(rebellion_frequency)))
+    # print("Average of REBELLION_FREQUENCY: ",
+    #       reduce(lambda x, y: x + y, rebellion_frequency) / (1.0 * len(rebellion_frequency)))
     print()
     return rebellion_frequency
 
@@ -105,8 +127,8 @@ def find_rebellion_active_average(rebellions):
     for t, actives in rebellions:
         rebellion_active_average.append(reduce(lambda x, y: x + y, actives) / (1.0 * len(actives)))
     print("REBELLION_ACTIVE_AVG", rebellion_active_average)
-    print("Average of REBELLION_ACTIVE_AVG: ",
-          reduce(lambda x, y: x + y, rebellion_active_average) / (1.0 * len(rebellion_active_average)))
+    # print("Average of REBELLION_ACTIVE_AVG: ",
+    #       reduce(lambda x, y: x + y, rebellion_active_average) / (1.0 * len(rebellion_active_average)))
     print()
     return rebellion_active_average
 
@@ -116,8 +138,8 @@ def find_rebellion_active_max(rebellions):
     for t, actives in rebellions:
         rebellion_active_max.append(max(actives))
     print("REBELLION_ACTIVE_MAX", rebellion_active_max)
-    print("Average of REBELLION_ACTIVE_MAX: ",
-          reduce(lambda x, y: x + y, rebellion_active_max) / (1.0 * len(rebellion_active_max)))
+    # print("Average of REBELLION_ACTIVE_MAX: ",
+    #       reduce(lambda x, y: x + y, rebellion_active_max) / (1.0 * len(rebellion_active_max)))
     print()
     return rebellion_active_max
 
@@ -125,51 +147,65 @@ def find_rebellion_active_max(rebellions):
 def draw_diagrams(df, rebellion_duration, rebellion_frequency, rebellion_active_average, rebellion_active_max,
                   from_model):
     n_bins = 12
-    x = rebellion_duration
 
     fig = plt.figure()
     # fig, axes = plt.subplots(nrows=2, ncols=2)
     # fig.suptitle(from_model, fontsize=10)
     # ax0, ax1, ax2, ax3 = axes.flatten()
+    x = rebellion_duration
     ax0 = fig.add_subplot(111)
     ax0.hist(x, n_bins, normed=1, histtype='bar', color='black')
     ax0.legend(prop={'size': 10})
-    ax0.set_title(from_model)
-    ax0.set_ylim([0, 1])
+    ax0_title = from_model + ', R_DUR Distribution'
+    ax0.set_title(ax0_title, fontsize=20)
+    ax0.set_xlabel("Rebellion Frequency", fontsize=20)
+    ax0.set_ylabel("Relative Frequency", fontsize=20)
+    ax0.set_ylim([0, 0.6])
 
     fig = plt.figure()
     x = rebellion_frequency
     ax1 = fig.add_subplot(111)
     ax1.hist(x, n_bins, normed=1, histtype='bar', color='black')
     ax1.legend(prop={'size': 10})
-    ax1.set_title(from_model)
-    ax1.set_xlabel("Rebellion Frequency")
-    ax1.set_ylabel("Relative Frequency")
+    ax1_title = from_model + ', R_FREQ Distribution'
+    ax1.set_title(ax1_title, fontsize=20)
+    ax1.set_xlabel("Rebellion Frequency", fontsize=20)
+    ax1.set_ylabel("Relative Frequency", fontsize=20)
     ax1.set_ylim([0, 0.06])
+    # fig.text(.15, .05, df_nl_header)
 
     fig = plt.figure()
     x = rebellion_active_average
     ax2 = fig.add_subplot(111)
     ax2.hist(x, n_bins, normed=1, histtype='bar', color='black')
     ax2.legend(prop={'size': 10})
-    ax2.set_title(from_model)
-    ax2.set_ylim([0, 0.02])
+    ax2_title = from_model + ', R_AVG Distribution'
+    ax2.set_title(ax2_title, fontsize=20)
+    ax2.set_xlabel("Rebellion Frequency", fontsize=20)
+    ax2.set_ylabel("Rebellion Average Size", fontsize=20)
+    ax2.set_ylim([0, 0.014])
 
     fig = plt.figure()
     x = rebellion_active_max
     ax3 = fig.add_subplot(111)
     ax3.hist(x, n_bins, normed=1, histtype='bar', color='black')
     ax3.legend(prop={'size': 10})
-    ax3.set_title(from_model)
-    ax3.set_ylim([0, 0.013])
+    ax3_title = from_model + ', R_MAX Distribution'
+    ax3.set_title(ax3_title, fontsize=20)
+    ax3.set_xlabel("Rebellion Frequency", fontsize=20)
+    ax3.set_ylabel("Relative Frequency", fontsize=20)
+    ax3.set_ylim([0, 0.0045])
 
     fig = plt.figure()
     time = range(0, 500)
     active_agents = df["ACTIVE"][0:500]
     ax5 = fig.add_subplot(111)
     ax5.plot(time, active_agents)
-    ax5.set_title(from_model)
-    ax5.set_ylim(0, 400)
+    ax5_title = from_model + ', N_ACTIVE Time Series'
+    ax5.set_title(ax5_title, fontsize=20)
+    ax5.set_xlabel("Time-Step", fontsize=20)
+    ax5.set_ylabel("Number of Active Agents", fontsize=20)
+    ax5.set_ylim(0, 200)
 
     fig.tight_layout()
     plt.show()
@@ -192,6 +228,6 @@ rebellion_active_max_j = find_rebellion_active_max(rebellions_j)
 print("======================================")
 
 draw_diagrams(df_nl, rebellion_duration_nl, rebellion_frequency_nl, rebellion_active_average_nl, rebellion_active_max_nl
-              , "NetLogo")
-# draw_diagrams(df_j, rebellion_duration_j, rebellion_frequency_j, rebellion_active_average_j, rebellion_active_max_j
-#              , "Java")
+               , "NetLogo")
+draw_diagrams(df_j, rebellion_duration_j, rebellion_frequency_j, rebellion_active_average_j, rebellion_active_max_j
+              , "Java")
